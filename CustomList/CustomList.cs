58f13ae983396a21871accdc12abfba56d955951
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-05 16:25:23 -0600

    Final commit of day. Set up member variables to aid in making Add method

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -8,5 +8,50 @@ namespace CustomList
 {
     public class CustomList<T>
     {
+        //member variables
+        private T[] items;
+        public int count { get; }
+        public int capacity { get; set; }
+
+        //below in constructor???
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                //logic
+            }
+        }
+
+
+
+
+        //constructor
+        public CustomList()
+        {
+            capacity = 4;
+            count = 0;
+            items = new T[capacity];
+        }
+
+        //member methods
+        public void Add(T item)
+        {
+
+
+        }
+
+        
+
     }
 }
