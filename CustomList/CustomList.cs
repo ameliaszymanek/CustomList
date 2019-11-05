3bdced930f7ef0bedb080bca91bf80ed07e39461
Amelia Szymanek <ameliaszymanek@Amelias-MacBook-Pro.local>
2019-11-05 11:54:49 -0600

    Commit before lunch. Working on .Add test methods

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    public class CustomList
+    public class CustomList<T>
     {
     }
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -8,8 +8,93 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CustomList_Add_AddOneValueToListCount()
         {
+            //arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expected = 1;
+            int actual;
+            int number1 = 3;
+
+            //act
+            list1.Add(number1);
+            actual = list1.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        
+        }
+
+        public void CustomList_Count_CountIs0WithNoValuesInList()
+        {
+            //arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            //act
+            actual = list1.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        public void CustomList_Add_AddingValuesThatResizeArray()
+        {
+            //arrange
+            CustomList<int> list1 = new CustomList<int>();
+            int expected = 5;
+            int actual;
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = 3;
+            int number4 = 4;
+            int number5 = 5;
+
+            //act
+            list1.Add(number1);
+            list1.Add(number2);
+            list1.Add(number3);
+            list1.Add(number4);
+            list1.Add(number5);
+            actual = list1.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void CustomList_Add_AddingStringValuesToListCount()
+        {
+            //arrange
+            CustomList<string> list1 = new CustomList<string>();
+            int expected = 1;
+            int actual;
+            string string1 = "hello";
+
+            //act
+            list1.Add(string1);
+            actual = list1.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void CustomList_Add_CheckIndexPositionOfValuesAdded()
+        {
+            //arrange
+            CustomList<string> list1 = new CustomList<string>();
+            int expected = "Alma";
+            int actual;
+            int string1 = "Amelia";
+            int string2 = "Andrew";
+            int string3 = "Alma";
+
+            //act
+
+
+            //Assert
+
         }
     }
 }
